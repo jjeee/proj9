2ff89bc44519475c9723f85b4d788963f5d763cf
jjeee <rofflemywofflepl0x@gmail.com>
2017-05-03 15:32:08 -0400

    1

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,8 @@ public class Main {
             i++;
         }
 
+        BinaryTreeInterface<Integer> emptyTree= new BinaryTree<>();
+        i=0;
         BinaryTreeInterface<Integer> aTree=new BinaryTree<>();
         aTree.setTree(numbers[i]);
         i++;
@@ -23,16 +25,16 @@ public class Main {
         bTree.setTree(numbers[i]);
         i++;
         BinaryTreeInterface<Integer> cTree=new BinaryTree<>();
-        cTree.setTree(numbers[i]);
+        cTree.setTree(numbers[i],aTree,bTree);
         i++;
         BinaryTreeInterface<Integer> dTree=new BinaryTree<>();
         dTree.setTree(numbers[i]);
         i++;
         BinaryTreeInterface<Integer> eTree=new BinaryTree<>();
-        eTree.setTree(numbers[i]);
+        eTree.setTree(numbers[i],dTree,emptyTree);
         i++;
         BinaryTreeInterface<Integer> fTree=new BinaryTree<>();
-        fTree.setTree(numbers[i]);
+        fTree.setTree(numbers[i],cTree,eTree);
         i++;
         BinaryTreeInterface<Integer> gTree=new BinaryTree<>();
         gTree.setTree(numbers[i]);
@@ -41,10 +43,10 @@ public class Main {
         hTree.setTree(numbers[i]);
         i++;
         BinaryTreeInterface<Integer> iTree=new BinaryTree<>();
-        iTree.setTree(numbers[i]);
+        iTree.setTree(numbers[i],gTree,hTree);
         i++;
         BinaryTreeInterface<Integer> jTree=new BinaryTree<>();
-        jTree.setTree(numbers[i]);
+        jTree.setTree(numbers[i],fTree,iTree);
         i++;
         BinaryTreeInterface<Integer> kTree=new BinaryTree<>();
         kTree.setTree(numbers[i]);
@@ -53,13 +55,13 @@ public class Main {
         lTree.setTree(numbers[i]);
         i++;
         BinaryTreeInterface<Integer> mTree=new BinaryTree<>();
-        mTree.setTree(numbers[i]);
+        mTree.setTree(numbers[i],kTree,lTree);
         i++;
         BinaryTreeInterface<Integer> nTree=new BinaryTree<>();
         nTree.setTree(numbers[i]);
         i++;
         BinaryTreeInterface<Integer> oTree=new BinaryTree<>();
-        oTree.setTree(numbers[i]);
+        oTree.setTree(numbers[i],nTree,mTree);
         i++;
         BinaryTreeInterface<Integer> pTree=new BinaryTree<>();
         pTree.setTree(numbers[i]);
@@ -68,18 +70,91 @@ public class Main {
         qTree.setTree(numbers[i]);
         i++;
         BinaryTreeInterface<Integer> rTree=new BinaryTree<>();
-        rTree.setTree(numbers[i]);
+        rTree.setTree(numbers[i],pTree,qTree);
         i++;
         BinaryTreeInterface<Integer> sTree=new BinaryTree<>();
-        sTree.setTree(numbers[i]);
+        sTree.setTree(numbers[i],rTree,oTree);
         i++;
         BinaryTreeInterface<Integer> tTree=new BinaryTree<>();
-        tTree.setTree(numbers[i]);
+        tTree.setTree(numbers[i],jTree,sTree);
 
 
 
 
+        Scanner userInput=new Scanner(System.in);
+        int choiceNum;
+        do {
+            System.out.println("Choose the following:\n1 - Add a number into the tree\n" +
+                "2 - Delete a number from the tree\n3 - Preorder Traversal\n4 - Inorder Traversal\n" +
+                "5 - Postorder Traversal\n6 - Exit");
 
+            choiceNum=userInput.nextInt();
+            switch (choiceNum){
+                case 1:
+                    System.out.println("Add: ");
+                    int add=userInput.nextInt();
+
+                    preorder(tTree);
+                    inorder(tTree);
+                    postorder(tTree);
+                    System.out.println();
+                    break;
+                case 2:
+                    System.out.println("Delete: ");
+                    int delete=userInput.nextInt();
+
+                    preorder(tTree);
+                    inorder(tTree);
+                    postorder(tTree);
+                    System.out.println();
+                    break;
+                case 3:
+                    preorder(tTree);
+                    System.out.println();
+                    break;
+                case 4:
+                    inorder(tTree);
+                    System.out.println();
+                    break;
+                case 5:
+                    postorder(tTree);
+                    System.out.println();
+                    break;
+                case 6:
+                    System.out.println("Bye");
+                    break;
+                default:
+                    System.out.println("Input is invalid");
+                    break;
+            }
+
+        }while (choiceNum!=6);
+
+
+    }
+
+    static void preorder(BinaryTreeInterface tTree){
+        Iterator<Integer> preorder=tTree.getPreorderIterator();
+        System.out.print("Preorder Traversal:\t");
+        while (preorder.hasNext())
+            System.out.print(preorder.next()+" ");
+        System.out.println();
+    }
+
+    static void inorder(BinaryTreeInterface tTree){
+        Iterator<Integer> inorder=tTree.getInorderIterator();
+        System.out.print("Inorder Traversal:\t");
+        while (inorder.hasNext())
+            System.out.print(inorder.next()+" ");
+        System.out.println();
+    }
+
+    static void postorder(BinaryTreeInterface tTree){
+        Iterator<Integer> postorder=tTree.getPostorderIterator();
+        System.out.print("Postorder Traversal:\t");
+        while (postorder.hasNext())
+            System.out.print(postorder.next()+" ");
+        System.out.println();
     }
 
     static void createNumbers() throws IOException {
